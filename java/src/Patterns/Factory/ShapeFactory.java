8607051035b8196f38c1e156fefc2967e1d548f4
[Your <amir.musairov96@gmail.com>
2021-03-11 11:16:14 +0200

    Add Changes to Factory pattern

--- java/src/Patterns/Factory/ShapeFactory.java
+++ java/src/Patterns/Factory/ShapeFactory.java
@@ -3,17 +3,15 @@ package Patterns.Factory;
 public class ShapeFactory {
 
     public Shape getShape(String shapeType) {
-        if (shapeType == null) {
-            return null;
+        switch(shapeType) {
+            case "Rectangle":
+                return new Rectangle();
+            case "Square":
+                return new Square();
+            case "Circle":
+                return new Circle();
+            default:
+                return null;
         }
-        if (shapeType.equalsIgnoreCase("Rectangle")) {
-            return new Rectangle();
-        } else if (shapeType.equalsIgnoreCase("Square")) {
-            return new Square();
-        } else if (shapeType.equalsIgnoreCase("Circle")) {
-            return new Circle();
-
-        }
-        return null;
     }
 }
